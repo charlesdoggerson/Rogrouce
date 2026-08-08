a059bb4c971baa36566d3aa65400871db9968444
charlesdoggerson <rmarios2009@gmail.com>
2026-08-08 17:35:30 +0300

    Update data.js

--- out/html/data.js
+++ out/html/data.js
@@ -5,9 +5,42 @@ const colourList = [{
         style: "color: #591716; font-weight: bold;"
     },
     {
-        word: "UNP",
-        style: "color: #006400; font-weight: bold;"
+        word: "BF",
+        style: "color: #2E7D32; font-weight: bold;"
     },
+    {
+        word: "Peasant Front",
+        style: "color: #2E7D32; font-weight: bold;"
+    },
+    {
+        word: "RLRP",
+        style: "color: #DAB74C; font-weight: bold;"
+    },
+    {
+        word: "SU",
+        style: "color: #E53935; font-weight: bold;"
+    },
+    {
+        word: "FVA",
+        style: "color: #2B0303; font-weight: bold;"
+    },
+    {
+        word: "RNU",
+        style: "color: #E57373; font-weight: bold;"
+    },
+    {
+        word: "LCPR",
+        style: "color: #F57C00; font-weight: bold;"
+    },
+    {
+        word: "RNKP",
+        style: "color: #303F9F; font-weight: bold;"
+    },
+     {
+        word: "NPF",
+        style: "color: #654D5AF; font-weight: bold;"
+    },                   
+                 
 ];
 const tooltipList = [{
     searchString: "RKP",
